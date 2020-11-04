75400bce1cff61ab7c07518cbd7e45a92d379f06
kocamane18 <72192226+kocamane18@users.noreply.github.com>
2020-11-04 18:32:30 +0300

    Add files via upload

---
+++ Number Prediction Game/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace Number_Prediction_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the number between 1-999.");
+
+            int answer, n1 = 0, counter = 0;
+            Random rastgele = new Random();
+            answer = rastgele.Next(1, 1000);
+
+            Console.WriteLine();
+
+            while(n1 != answer)
+            {
+                Console.Write("Guess the number:");
+                n1 = Convert.ToInt16(Console.ReadLine());
+
+                if (n1 < answer)
+                    Console.WriteLine("Go up.");
+                else if (n1 > answer)
+                    Console.WriteLine("Go down.");
+
+                counter++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The number of tryings:{0}", counter);
+
+
+        }
+    }
+}
---
+++ Playing Music/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace Playing_Music
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer music = new SoundPlayer();
+            string access = "C:\\Users\\CASPER\\Desktop\\Welcome to the Show.wav"; // parantezden önce @ da koyabilirdik.
+            music.SoundLocation = access;
+            music.Play();
+
+            Console.Read();
+        }
+    }
+}
